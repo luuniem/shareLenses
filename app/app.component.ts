76d578c786ec67dcd49d9bb2423a245725f71b70
Niem Nguyen <luuniem87@gmail.com>
2016-08-20 18:11:57 -0700

    add Sass files

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from 'angular2/core';
 
 @Component({
   selector: 'my-app',
-  template: `
+  template:
+  `
     <div class="container">
+    <header>
       <h1>Skeleton Angular2 App!</h1>
+      <ul>
+        <li>I like red</li>
+        <li>I like green</li>
+        <li>I like blue</li>
+      </ul>
+      </header>
     </div>
   `
 })
